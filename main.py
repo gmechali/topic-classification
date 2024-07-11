a0883fb35dbc5d556fea6f9f8c5679c4405e41f0
Gabriel Mechali <gabriel.mechali@gmail.com>
2024-07-11 08:57:52 -0400

    Change training date to remove per topic aggregation, instead have rows of names to topics. Use train_test_split on these rows. Change from MultinomialNb to Logistic Regression as it showed significantly higher performance

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import datacommons_pandas as dc
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
@@ -13,7 +14,7 @@ from absl import flags
 
 FLAGS = flags.FLAGS
 flags.DEFINE_boolean('create_training_data_set', False, 'Outputs a training data set')
-flags.DEFINE_string('training_data_set_filename', 'topic_to_names', 'Filename to use for training data.')
+flags.DEFINE_string('training_data_set_filename', 'names_to_topics', 'Filename to use for training data.')
 flags.DEFINE_boolean('evaluate_model', False, 'Outputs predictions for test data')
 flags.DEFINE_boolean('test_accuracy', False, 'Whether to test accuracy with test_train_split.')
 flags.DEFINE_string('test_data_set_path', 'dc_sample_data.csv', 'Path to the csv file to use for training data.')
@@ -81,7 +82,7 @@ def get_names_from_topics(dc_topics):
 	# topics_to_stat_vars is a dict containing the topic as the key, and the list of all
 	# associated statVars as the value.
 
-	topics_to_names = {}
+	names_to_topics = {}
 	for topic in topics_to_stat_vars:
 		if topics_to_stat_vars[topic]:
 			# Fetch names of all statVars
@@ -89,10 +90,12 @@ def get_names_from_topics(dc_topics):
 			stat_var_names = []
 			for stat_var in response:
 				stat_var_names.extend(response[stat_var])
-			topics_to_names[topic] = "; ".join(stat_var_names)
+
+			for name in stat_var_names:
+				names_to_topics[name] = topic
 	
-	# Formatted as: "dc/topic/Demographics": "Total Population; Population Density; Rate of Population Growth" (3 statVars)
-	return topics_to_names
+	# Formatted as: "Total Population": "dc/topic/Demographics"
+	return names_to_topics
 
 def fetch_training_data():
 	print("Fetching training data")
@@ -104,17 +107,17 @@ def fetch_training_data():
 
 	# DC API marks a limit of 500 nodes per API call so we should be able to get all the topic metadata in one.
 	# Note we're duping calls that were already made for simplicity then.
-	topics_to_names = get_names_from_topics(dc_topics)
+	names_to_topics = get_names_from_topics(dc_topics)
 
 	# Write the final output of Topics to list of names for associated variable into a file, so we don't have to always
 	# regenerate this!
 	with open(FLAGS.training_data_set_filename+'.json', 'w') as convert_file: 
-		convert_file.write(json.dumps(topics_to_names))
+		convert_file.write(json.dumps(names_to_topics))
 
-	# Formatted as: "dc/topic/Demographics": "Total Population; Population Density; Rate of Population Growth"
+	# Formatted as: "Total Population": "dc/topic/Demographics"
 	# Keeping it since it's more human-readable.
 	with open(FLAGS.training_data_set_filename + '.txt', 'w') as f:
-		for key, value in topics_to_names.items():
+		for key, value in names_to_topics.items():
 			f.write('%s:%s\n' % (key, value))
 
 
@@ -125,31 +128,28 @@ def train_model():
 	f = open(FLAGS.training_data_set_filename+'.json')
 	data = json.load(f)
 
-	X_train = []
-	y_train = []
-	for topic in data:
-		y_train.append(topic)
-		X_train.append(data[topic])
+	X = []
+	y = []
+	for name in data:
+		X.append(name)
+		y.append(data[name])
 
-	X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.5)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-	vectorizer = CountVectorizer(stop_words="english", analyzer="word")
+	vectorizer = CountVectorizer(stop_words="english", analyzer="word", ngram_range=(1,3))
 
 	X_train = vectorizer.fit_transform(X_train)
 
 	# MNB CLASSIFICATION
-	mnb = MultinomialNB()
-	mnb.fit(X_train,y_train)
+	model = LogisticRegression()
+	model.fit(X_train,y_train)
 
 	if FLAGS.test_accuracy:
-		print(X_test)
 		X_test = vectorizer.transform(X_test)
-		test_prediction = mnb.predict(X_test)
-		print(y_test)
-		print(test_prediction)
-		print("Accuracy score: ", accuracy_score(y_test, test_prediction))
+		test_prediction = model.predict(X_test)
+		print("Accuracy: ", accuracy_score(y_test, test_prediction))
 
-	return mnb, vectorizer
+	return model, vectorizer
 
 def evaluate():
 	"""Evaluates a given model using test data. We first try based on the `Name` test data, and fall back to `Chart Title`
@@ -171,7 +171,6 @@ def evaluate():
 
 	# Store the predictions in dictionary. Keep the Names prediction when available. Fall back to Chart Title.
 	index = 0
-	fail = 0
 	predictions = {}
 	for pr in name_prediction:
 		if name_samples[index] != "nan":
