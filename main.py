71c29f49fb465b4371b645aa8811ea233b3a8a44
Gabriel Mechali <gabriel.mechali@gmail.com>
2024-07-10 21:28:05 -0400

    Cleanup unused code and add method comments all over.

--- main.py
+++ main.py
@@ -14,12 +14,8 @@ def filter_topics(variable):
 	return False
 
 def filter_statVars(variable):
-	return not filter_topics(variable) and not filter_statVarGroups(variable) and not filter_statVarPeerGroups(variable)
-
-def filter_statVarGroups(variable):
-	if variable.startswith("dc/svg"):
-		return True
-	return False
+	# TODO(gmechali): Confirm there are no StatVarGroups associated with topics.
+	return not filter_topics(variable) and not filter_statVarPeerGroups(variable)
 
 def filter_statVarPeerGroups(variable):
 	if variable.startswith("dc/svpg"):
@@ -27,19 +23,19 @@ def filter_statVarPeerGroups(variable):
 	return False
 
 def get_topics():
+	"""Fetches all topics in the Data Commons Graph, starting at dc/topic/Root
+	and iterating through it using BFS. Topics have subtopics via the relevantVariable
+	property.
+	"""
 	dc_topics = []
 
 	next_level_topics = ['dc/topic/Root']
 	while len(next_level_topics) > 0:
-		# TODO: Maybe use relevantVariableList
 		response = dc.get_property_values(next_level_topics,'relevantVariable')
 		next_level_topics.clear()
 
-		new_topics = []
 		for topic in response:
-			new_topics = response[topic]
-			new_topics = list(filter(filter_topics, new_topics))
-			next_level_topics.extend(new_topics)
+			next_level_topics.extend(list(filter(filter_topics, response[topic])))
 
 		if len(next_level_topics) > 0:
 			dc_topics.extend(next_level_topics)
@@ -47,38 +43,49 @@ def get_topics():
 	return dc_topics
 
 def get_names_from_topics(dc_topics):
+	"""Fetch the names of all statVars associated to the list of topics passed in.
+	We rely on the statVars directly associated with topics, or associated indirectly
+	via a StatVarPeerGroup.
+	"""
 	response = dc.get_property_values(dc_topics,'relevantVariable')
 	topics_to_stat_vars = {}
 
-	count = 0
 	for topic in response:
-		# get all non-topic relevant variables
+		# Gets all statVars and StatVarPeerGroups from the response.
 		stat_vars = list(filter(filter_statVars, response[topic]))
-		stat_var_groups = list(filter(filter_statVarGroups, response[topic]))
 		stat_var_peer_groups = list(filter(filter_statVarPeerGroups, response[topic]))
 
-		# Confirmed that there are not StatVarGroups, only SVPGs
 		if stat_var_peer_groups:
+			# Fetch statVars associated with the statVarPeerGroups found.
 			svpg_resp = dc.get_property_values(stat_var_peer_groups,'member')
 			for svpg in svpg_resp:
 				stat_vars.extend(svpg_resp[svpg])
 
-		# TODO what if double nestedness
+		# TODO(gmechali): Confirm if there can be double nestedness. 
 		topics_to_stat_vars[topic] = stat_vars
 
 	print("Done fetching topic to stat vars")
+
+	# topics_to_stat_vars is a dict containing the topic as the key, and the list of all
+	# associated statVars as the value.
+
 	topics_to_names = {}
 	for topic in topics_to_stat_vars:
 		if topics_to_stat_vars[topic]:
+			# Fetch names of all statVars
 			response = dc.get_property_values(topics_to_stat_vars[topic],'name')
 			stat_var_names = []
 			for stat_var in response:
 				stat_var_names.extend(response[stat_var])
 			topics_to_names[topic] = "; ".join(stat_var_names)
-	# Formatted as: "dc/topic/Demographics": "Total Population; Population Density; Rate of Population Growth"
+	
+	# Formatted as: "dc/topic/Demographics": "Total Population; Population Density; Rate of Population Growth" (3 statVars)
 	return topics_to_names
 
 def train_model():
+	"""Train the model using json output.
+	Returns the model and the CountVectorizer. 
+	"""
 	f = open('topic_to_names.json')
 	data = json.load(f)
 
@@ -101,38 +108,37 @@ def train_model():
 def fetch_training_data():
 	print("Fetching training data")
 
-	# print("Fetching all topics")
-	# dc_topics = get_topics()
+	print("Fetching all topics")
+	dc_topics = get_topics()
 
-	# print("Now we have found all DataCommons topics, for a total topic count of ", len(dc_topics))
+	print("Now we have found all DataCommons topics, for a total topic count of ", len(dc_topics))
 
 	# DC API marks a limit of 500 nodes per API call so we should be able to get all the topic metadata in one.
 	# Note we're duping calls that were already made for simplicity then.
-	# topics_to_names = get_names_from_topics(dc_topics)
-
-	# process_training_data()
+	topics_to_names = get_names_from_topics(dc_topics)
 
 	# Write the final output of Topics to list of names for associated variable into a file, so we don't have to always
 	# regenerate this!
-	# with open('topic_to_names.json', 'w') as convert_file: 
-	# 	convert_file.write(json.dumps(topics_to_names))
+	with open('topic_to_names.json', 'w') as convert_file: 
+		convert_file.write(json.dumps(topics_to_names))
 
 	# Formatted as: "dc/topic/Demographics": "Total Population; Population Density; Rate of Population Growth"
-	# with open("topic_to_names.txt", 'w') as f:
-	# 	for key, value in topics_to_names.items():
-	# 		f.write('%s:%s\n' % (key, value))
+	# Keeping it since it's more human-readable.
+	with open("topic_to_names.txt", 'w') as f:
+		for key, value in topics_to_names.items():
+			f.write('%s:%s\n' % (key, value))
 
 
 def fetch_test_data():
-	# Columns we want to read. Some rows have no name but have a ChartTitle.
-	usecols = ["Name", "Chart Title", "StatVar"] # , "ConciseChartTitle"
-
-	# Store all columns selected in a data frame.
-	df = pd.read_csv("dc_sample_data.csv", usecols=usecols)
-
-	return df
+	"""Extract test data from sample data set. Use both the `Name` and `Chart Title` columns for classification.
+	Returns a dataframe with the selected data.
+	"""
+	return pd.read_csv("dc_sample_data.csv", usecols=["Name", "Chart Title", "StatVar"])
 
 def evaluate(model, vectorizer):
+	"""Evaluates a given model using test data. We first try based on the `Name` test data, and fall back to `Chart Title`
+	Outputs the prediction in a json file. 
+	"""
 	test_df = fetch_test_data()
 	chart_title_samples = test_df['Chart Title'].values.astype('U')
 	X_test = vectorizer.transform(chart_title_samples)
